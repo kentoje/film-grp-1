dadf2a7f2e62be40c65db3ef1860463c2f426b92
kentoje <k.monthubert@gmail.com>
2021-02-26 10:23:37 +0100

    change style

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,6 @@
 import React, { useContext } from 'react'
 import { ScrollView } from 'react-native'
-import { 
-  Text, 
-  Image, 
-  StyleSheet, 
-  View,
-  Button,
-  Dimensions,
-} from 'react-native'
+import { Text, Image, StyleSheet, View, Button, Dimensions, Platform } from 'react-native'
 import { Icon } from 'react-native-elements'
 import { useParams } from 'react-router-native'
 import GoBack from '../components/GoBack'
@@ -15,7 +8,9 @@ import moviesContext from '../context/MoviesContext'
 import getResponsiveImageDimension from '../lib/responsive-image'
 
 const RATIO = 0.25
-const responsiveDimension = getResponsiveImageDimension(RATIO)(Dimensions.get('window').width)
+const responsiveDimension = getResponsiveImageDimension(RATIO)(
+  Dimensions.get('window').width,
+)
 
 const Movie = () => {
   const [apiMovies] = useContext(moviesContext)
@@ -24,38 +19,39 @@ const Movie = () => {
 
   return (
     <>
-      <Image
-        style={styles.image}
-        source={{ uri: movie.image }}
-      />
+      <Image style={styles.image} source={{ uri: movie.image }} />
       <View style={styles.arrow}>
         <GoBack />
       </View>
-      <View style={{flex: 1}}>
-        <ScrollView>
-          <View style={styles.container}>
-            <View style={styles.imageWrapper}>
-              <Image
-                  style={styles.thumbnail}
-                  resizeMode={'contain'}
-                  source={{ uri: movie.image }}
-              />
-            </View>
-            <View>
-              <Text style={styles.title}>{movie.title}</Text> 
-              <Text style={styles.author}>{movie.title}</Text>
-            </View>
-            <View style={styles.playIconContainer}>
-              <Icon color="#fff" size={48} name="arrow-right" />
-            </View>
+      <View style={styles.contentWrapper}>
+        <View style={styles.container}>
+          <View style={styles.imageWrapper}>
+            <Image
+              style={styles.thumbnail}
+              resizeMode={'cover'}
+              source={{ uri: movie.image }}
+            />
           </View>
+          <View style={styles.informations}>
+            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.author}>{movie.title}</Text>
+          </View>
+          <View style={styles.playIconContainer}>
+            <Icon color="#fff" size={48} name="arrow-right" />
+          </View>
+        </View>
+        <ScrollView>
           <View style={styles.synopsisContainer}>
             <Text style={styles.synopsis}>Synopsis</Text>
             <Text>{movie.overview}</Text>
           </View>
         </ScrollView>
         <View style={styles.buttonWrapper}>
-          <Button color="#fff" title="View website" />
+          {Platform.OS === 'ios' ? (
+            <Button color="#fff" title="View website" />
+          ) : (
+            <Text style={{ color: '#fff' }}>View Website</Text>
+          )}
         </View>
       </View>
     </>
@@ -67,26 +63,41 @@ const styles = StyleSheet.create({
     height: responsiveDimension,
     flexDirection: 'row',
   },
+  contentWrapper: {
+    flex: 1,
+    marginTop: -24,
+  },
   arrow: {
-    alignSelf: 'flex-start',
+    position: 'absolute',
+    top: 8,
+    left: 8,
   },
   image: {
     height: 200,
     width: '100%',
   },
   imageWrapper: {
-    width: responsiveDimension,
+    width: responsiveDimension * 0.8,
+    marginLeft: 16,
+    borderWidth: 3,
+    borderColor: '#fff',
+    borderRadius: 6,
   },
   thumbnail: {
     flex: 1,
+    borderRadius: 6,
   },
   playIconContainer: {
     width: 50,
     height: 50,
-    backgroundColor: 'red',
+    backgroundColor: '#f44802',
     borderRadius: 24,
     marginLeft: 'auto',
     marginRight: 16,
+  },
+  informations: {
+    marginTop: 32,
+    marginLeft: 8,
   },
   title: {
     fontWeight: 'bold',
@@ -100,10 +111,12 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   buttonWrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
     width: '90%',
-    paddingVertical: 8,
+    paddingVertical: 10,
     marginHorizontal: 16,
-    backgroundColor: 'red',
+    backgroundColor: '#f44802',
     color: 'white',
     borderRadius: 12,
   },
